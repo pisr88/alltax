9e446f41cb59830d68ebd04ee62cbb0b32ea1808
Piotr S <piotr.sroka@outlook.com>
2022-03-20 23:27:05 +0100

    add nav btn and animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
-const test = () => {
-	console.log('test')
+const burgerBtn = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav')
+const navItem = document.querySelectorAll('.nav-item')
+
+const showNav = () => {
+	burgerBtn.classList.toggle('is-active')
+	nav.classList.toggle('show-nav')
+	navItem.forEach(el => {
+		console.log(el)
+		el.classList.toggle('show-nav')
+	})
 }
+
+burgerBtn.addEventListener('click', showNav)
