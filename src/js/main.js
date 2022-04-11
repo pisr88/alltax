acd4f8871d37220995dd5aa4a07051af2911ca9f
Piotr S <piotr.sroka@outlook.com>
2022-04-11 21:25:17 +0200

    test

--- src/js/main.js
+++ src/js/main.js
@@ -127,3 +127,31 @@ const handleCurrentYear = () => {
 }
 
 handleCurrentYear()
+
+// Pricelist - API form CMS 
+fetch('https://api-eu-central-1.graphcms.com/v2/cl1qea3851j6901z76wz6foyx/master', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		Accept: 'application/json',
+	},
+	body: JSON.stringify({
+		query: `
+                    query {
+                    offers {
+                        name
+                        scope
+                        conditions
+                    }
+                    }
+                `,
+	}),
+})
+	.then(res => {
+		if (!res.ok) return Promise.reject(response)
+
+		return res.json()
+	})
+	.then(res => {
+		console.log(res.data)
+	}) 
