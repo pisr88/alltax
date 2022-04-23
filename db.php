39a360e5ce338f54b2ea923e1d55ad1396fd3581
Sroks <lukasz.sroka@poczta.fm>
2022-04-23 17:35:10 +0200

    php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
     $servername='localhost';
-    $username='Admin';
-    $password='Admin123';
-    $dbname = "Kontakty";
+    $username='root';
+    $password='';
+    $dbname = "users";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
